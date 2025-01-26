85b9e04953c498c9567a9d4daeeada1e173c226b
Blurlingite <vkdawah@gmail.com>
2025-01-25 22:45:06 -0500

    Add dynamic routes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <Link href="/meals">Meal Page</Link>
+      <Link href="/meals/share">Meal Share Page</Link>
+      <Link href="/community">Community Page</Link>
     </main>
   );
 }
